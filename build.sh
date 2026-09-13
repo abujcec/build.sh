c8a639cd0283010eb658a411ba541b1de567364c
abujcec <alen.loncar14@gmail.com>
2026-09-13 17:17:36 +0200

    Add device tree existence check in build script
    Added checks to clone device tree if missing.

--- build.sh
+++ build.sh
@@ -27,3 +27,12 @@ lunch lineage_${DEVICE}-ap4a-userdebug
 
 # Run compilation
 mka bacon -j$(nproc --all)
+
+# Check if device tree exists
+if [ ! -d "device/xiaomi/veux" ]; then
+    echo "Device tree missing! Cloning manually..."
+    git clone https://github.com/xiaomi-sm6375-devs/android_device_xiaomi_veux.git device/xiaomi/veux -b lineage-23.2 --depth=1
+    git clone https://github.com/xiaomi-sm6375-devs/android_device_xiaomi_sm6375-common.git device/xiaomi/sm6375-common --depth=1
+    git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth=1
+    git clone https://github.com/xiaomi-sm6375-devs/android_kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
+fi
