800966361858c49543f8bf8b4bcc629bf14e1113
abujcec <alen.loncar14@gmail.com>
2026-09-14 06:48:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,6 +24,20 @@ if [ ! -d "device/xiaomi/veux" ]; then
     git clone https://github.com/xiaomi-sm6375-devs/android_kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
 fi
 
+# Get vendor blobs
+if [ ! -d "vendor/xiaomi/veux" ]; then
+    echo "Getting vendor blobs..."
+    mkdir -p vendor_dump/vendor
+    wget --no-check-certificate "https://drive.google.com/uc?export=download&id=14gZlIb5q4BYgShBmo7F4G499x09Qs972" -O vendor.img
+    sudo apt-get install -y e2fsprogs
+    mkdir -p vendor_dump/vendor
+    debugfs -R "rdump / vendor_dump/vendor" vendor.img
+    PYTHONPATH=tools/extract-utils python3 device/xiaomi/veux/extract-files.py vendor_dump
+    PYTHONPATH=tools/extract-utils python3 device/xiaomi/veux/extract-files.py -m
+    PYTHONPATH=tools/extract-utils python3 device/xiaomi/sm6375-common/extract-files.py -m
+    rm -f vendor.img
+fi
+
 source build/envsetup.sh
 lunch lineage_${DEVICE}-ap4a-userdebug
 mka bacon -j$(nproc --all)
