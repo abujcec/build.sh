84fb04da8d4998724d1e539df858b85d7b45c4f7
abujcec <alen.loncar14@gmail.com>
2026-09-18 18:56:00 +0200

    Fix kernel config and update UprobeStats handling
    Updated kernel configuration and removed UprobeStats dependencies.

--- build.sh
+++ build.sh
@@ -21,13 +21,13 @@ if [ ! -d "device/xiaomi/veux" ]; then
     git clone https://github.com/xiaomi-sm6375-devs/android_device_xiaomi_sm6375-common.git device/xiaomi/sm6375-common --depth=1
     git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth=1
     git clone https://github.com/xiaomi-sm6375-devs/android_kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
-    # Fix kernel config
     sed -i 's/TARGET_KERNEL_CONFIG := veux_defconfig/TARGET_KERNEL_CONFIG := gki_defconfig vendor\/holi_GKI.config/' device/xiaomi/veux/BoardConfig.mk
 fi
 
-# Fix UprobeStats and its dependencies
+# Fix UprobeStats
 rm -rf packages/modules/UprobeStats
-rm -rf cts/hostsidetests/statsdatom
+sed -i '/"uprobestats_mainline_flags_java_lib_test_host"/d' cts/hostsidetests/statsdatom/Android.bp
+sed -i '/"uprobestats-protos"/d' cts/hostsidetests/statsdatom/Android.bp
 
 if [ ! -d "vendor/xiaomi/veux" ]; then
     echo "Getting vendor blobs..."
